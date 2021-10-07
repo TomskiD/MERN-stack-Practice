0ea3e75eeda702e06c72235d7455fcbdf54ccedd
Tomasz Dmowski <dmowski.tomek@gmail.com>
2021-10-07 22:48:56 +0200

    connect to DB using mongoose

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
+const homeRouter = require("./routes/home");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to MERN practice");
-});
+app.use("/", homeRouter)
+// connect to database
+mongoose.connect(process.env.DB_CONNECTION, () =>
+  console.log("Connected to DB!")
+);
 
+// listen
 app.listen(3000);
+
+const password = "2qG2dAgfjfHowpcN";
