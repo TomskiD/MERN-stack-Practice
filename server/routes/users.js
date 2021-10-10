344941fff04eac2414cfd0e9c7b692c5d009af37
Tomasz Dmowski <dmowski.tomek@gmail.com>
2021-10-10 20:51:29 +0200

    setup controllers

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-import getUsers from "../models/user/userController.js";
+import { getUsers, createUser } from "../models/user/userController.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.post("/", createUser);
 
 export default router;
