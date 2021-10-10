2b8c691d91526cb8a0347ae42178b854b0856227
Tomasz Dmowski <dmowski.tomek@gmail.com>
2021-10-10 23:00:50 +0200

    initialization

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Form from "./components/Form/Form";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Form></Form>
+    </React.Fragment>
+  );
+};
+
+export default App;
