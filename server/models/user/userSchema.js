b94c55febea159f60a9a8ab11bd40440412b4ae4
Tomasz Dmowski <dmowski.tomek@gmail.com>
2021-10-10 20:52:16 +0200

    POST: simplify validation

--- server/models/user/userSchema.js
+++ server/models/user/userSchema.js
@@ -1,27 +1,16 @@
 import mongoose from "mongoose";
 
-import { validateEmail, validateName } from "./userValidation";
-
 const UserSchema = mongoose.Schema({
   firstName: {
     type: String,
-    required: [true, "Please, enter your first name."],
-    validate: [validateName, "Please fill a valid last name"],
     trim: true,
   },
   lastName: {
     type: String,
-    required: [true, "Please, enter your last name."],
-    validate: [validateName, "Please fill a valid last name"],
-    trim: true,
   },
   email: {
     type: String,
     trim: true,
-    lowercase: true,
-    unique: true,
-    required: "Email address is required",
-    validate: [validateEmail, "Please fill a valid email address"],
   },
 });
 
