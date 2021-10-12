06ddbcda70f756c5e3753f5dfb79fe7305f2ee86
Tomasz Dmowski <tdmowski-at-985591258337>
2021-10-12 16:09:06 +0200

    extract form value/fields

---
+++ client/src/components/Form/formFieldsValue.js
@@ -0,0 +1,26 @@
+export const inputFieldValues = [
+  {
+    name: "firstName",
+    label: "First Name",
+    id: "my-first-name"
+  },
+  {
+    name: "lastName",
+    label: "Last Name",
+    id: "my-last-name"
+  },
+  {
+    name: "email",
+    label: "Email",
+    id: "my-email"
+  },
+];
+
+export const initialFormValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  date: "",
+  formSubmitted: false,
+  success: false,
+}
