bb028f5e8b4fdb54a114b23fb2d7b45b30207f8d
Tomasz Dmowski <dmowski.tomek@gmail.com>
2021-11-02 22:42:39 +0100

    Error handling of writing the same email to the database

--- client/src/actions/events.js
+++ client/src/actions/events.js
@@ -10,11 +10,13 @@ export const getEvents = () => async (dispatch) => {
   }
 };
 
-export const createEvent = (user) => async (dispatch) => {
-  try {
-    const { data } = await api.createEvent(user);
-    dispatch({ type: CREATE, payload: data });
-  } catch (err) {
-    console.log(err.message);
+export const createEvent = (event) => async (dispatch) => {
+  const response = await api.createEvent(event);
+  const duplicateKeyError = 11000;
+  if (response?.data?.error?.code === duplicateKeyError) {
+    alert('The given e-mail address already exists in the database.');
+    return;
   }
+  dispatch({ type: CREATE, payload: response });
+  alert('The event has been successfully registered in the database.');
 };
